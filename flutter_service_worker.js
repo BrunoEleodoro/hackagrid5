009f9f4b6c76223baebd1a45750a80dd8a127c41
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-08-23 12:45:31 -0300

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "5ece4e86ec9ae965725eede204b4f44e",
 "/": "5ece4e86ec9ae965725eede204b4f44e",
-"main.dart.js": "8cf6fd3364930f98c4faaca9decb5118",
+"main.dart.js": "1b5bf663069f5f349a1fb8fccdd0228d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
